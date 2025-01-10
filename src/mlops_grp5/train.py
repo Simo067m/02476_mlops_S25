e7a01f13dbbac24daf875a36f50d534a26075ea8
Simon Stenbæk Jensen <s214592@student.dtu.dk>
2025-01-10 17:43:21 +0900

    Update train.py for directory robustness

--- src/mlops_grp5/train.py
+++ src/mlops_grp5/train.py
@@ -1,18 +1,24 @@
 import torch
+import os
 from model import PlaceHolderModel
 from pytorch_lightning import Trainer
 from visualize import plot_placeholder_loss
 
-from data import get_placeholder_dataloader
+from dataloaders import get_placeholder_dataloader
 
 if __name__ == "__main__":
-    trainer = Trainer(max_epochs=10, accelerator="gpu", devices=1, logger=False, enable_checkpointing=False)
+    device = "gpu" if torch.cuda.is_available() else "cpu"
+    trainer = Trainer(max_epochs=10, accelerator=device, devices=1, logger=False, enable_checkpointing=False)
     model = PlaceHolderModel()
     train_loader, test_loader = get_placeholder_dataloader()
     trainer.fit(model, train_loader)
     trainer.test(model, test_loader)
     print("Training and testing complete.")
+    if not os.path.exists("models"):
+        os.makedirs("models")
     torch.save(model.state_dict(), "models/model.pth")
     print("Model saved.")
+    if not os.path.exists("reports/figures"):
+        os.makedirs("reports/figures")
     plot_placeholder_loss()
     print("Loss plot saved.")
