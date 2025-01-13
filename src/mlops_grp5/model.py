3567829d671f49d24de2756db637f112c04bd639
Simon Stenbæk Jensen <s214592@student.dtu.dk>
2025-01-13 16:49:23 +0900

    Add pytorch-image-models PyPi package timm

--- src/mlops_grp5/model.py
+++ src/mlops_grp5/model.py
@@ -2,5 +2,9 @@ import pytorch_lightning as pl
 import torch
 import torch.nn as nn
 
+import timm
+
 if __name__ == "__main__":
-    raise NotImplementedError()
+    model = timm.create_model("resnet50_clip.openai", pretrained=True, num_classes=2)
+    model.eval()
+    
