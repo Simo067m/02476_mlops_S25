8084bf4a568bc30259d728fb4a75549950548e79
Simon Stenbæk Jensen <s214592@student.dtu.dk>
2025-01-13 22:12:52 +0900

    Add model inference

--- src/mlops_grp5/model.py
+++ src/mlops_grp5/model.py
@@ -3,8 +3,30 @@ import torch
 import torch.nn as nn
 
 import timm
+from torchvision import transforms
+from PIL import Image
+
+def get_model(model_name: str, num_classes: int) -> nn.Module:
+    """Gets a pretrained image model from the timm library."""
+    model = timm.create_model(model_name, pretrained=True, num_classes=num_classes)
+    return model
+
+def model_forward(model: nn.Module, imgs: torch.Tensor) -> torch.Tensor:
+    """Makes a prediction using a model."""
+    # Make predictions
+    model.eval()  # Set model to evaluation mode
+    with torch.no_grad():  # Disable gradient calculations
+        outputs = model(imgs)
+    
+    return outputs
 
 if __name__ == "__main__":
-    model = timm.create_model("resnet50_clip.openai", pretrained=True, num_classes=2)
-    model.eval()
-    
+    model_name = "resnet50_clip.openai"
+    model = get_model(model_name, num_classes=2)
+
+    image = Image.open("data/fruits_vegetables_dataset/Fruits/FreshApple/freshApple (1).jpg")
+
+    outputs = model_forward(model, image)
+
+    print(outputs)
+    print(outputs.shape)
