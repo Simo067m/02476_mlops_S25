d42f432f85f867c92edff2f09589a6a629b27bf5
Katharina-s214623 <s214634@student.dtu.dk>
2025-01-14 13:46:05 +0100

    Wandb logging

--- src/mlops_grp5/train.py
+++ src/mlops_grp5/train.py
@@ -3,18 +3,24 @@ import os
 from model import ImageModel
 from pytorch_lightning import Trainer
 from visualize import plot_placeholder_loss
+import wandb
+import pytorch_lightning as pl
 
 from dataloaders import get_fruits_and_vegetables_dataloaders
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+
+
 
 if __name__ == "__main__":
     print(f"Training on device:", DEVICE)
 
     # Define the model
     model = ImageModel(learning_rate=1e-3, weight_decay=1e-5).to(DEVICE)
+    #wandb.init(project="mlops-grp5", 
+    #        config={"learning_rate": 1e-3, "weight_decay": 1e-5, "batch_size": 32})
 
     # Define the trainer
-    trainer = Trainer(max_epochs=10, accelerator=DEVICE, devices=1, logger=False, enable_checkpointing=False)
+    trainer = Trainer(max_epochs=10, accelerator=DEVICE, devices=1, logger=pl.loggers.WandbLogger(project='mlops-grp5',config={"learning_rate": 1e-3, "weight_decay": 1e-5}), enable_checkpointing=False)
 
     # Load data
     train_loader, test_loader, val_loader = get_fruits_and_vegetables_dataloaders()
@@ -23,7 +29,7 @@ if __name__ == "__main__":
     trainer.fit(model, train_loader, val_loader)
     trainer.test(model, test_loader)
     print("Training and testing complete.")
-
+    
     # Save the model
     if not os.path.exists("models"):
         os.makedirs("models")
