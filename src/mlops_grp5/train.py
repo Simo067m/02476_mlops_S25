edbec31ab68983823120cf3b11f9e5227b0686c0
Katharina-s214623 <s214634@student.dtu.dk>
2025-01-14 17:37:01 +0100

    Created sweep functionality and typer interface

--- src/mlops_grp5/train.py
+++ src/mlops_grp5/train.py
@@ -5,21 +5,23 @@ from pytorch_lightning import Trainer
 from visualize import plot_placeholder_loss
 import wandb
 import pytorch_lightning as pl
+import typer
 
 from dataloaders import get_fruits_and_vegetables_dataloaders
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 
-
-
-if __name__ == "__main__":
+def train_model(learning_rate: float = 1e-3, weight_decay: float = 1e-5, max_epochs: int = 10):
     print(f"Training on device:", DEVICE)
     torch.cuda.empty_cache()
+    print('Training model with learning rate:', learning_rate, 'and weight decay:', weight_decay)
+    print('Learning_rate type:', type(learning_rate), 'Weight_decay type:', type(weight_decay))
+    #wait for input in terminal
     # Define the model
-    model = ImageModel(learning_rate=1e-3, weight_decay=1e-5).to(DEVICE)
+    model = ImageModel(learning_rate=learning_rate, weight_decay=weight_decay).to(DEVICE)
     checkpoint_callback = pl.callbacks.ModelCheckpoint(monitor='val_loss', mode='min')
     early_stop_callback = pl.callbacks.EarlyStopping(monitor='val_loss', patience=3, verbose=True, mode='min')
     # Define the trainer
-    trainer = Trainer(max_epochs=10, accelerator=DEVICE, devices=1, logger=pl.loggers.WandbLogger(project='mlops-grp5',config={"learning_rate": 1e-3, "weight_decay": 1e-5}, log_model=True), callbacks=[checkpoint_callback, early_stop_callback])
+    trainer = Trainer(max_epochs=max_epochs, accelerator=DEVICE, devices=1, logger=pl.loggers.WandbLogger(project='mlops-grp5', config={"learning_rate": learning_rate, "weight_decay": weight_decay}, log_model=True), callbacks=[checkpoint_callback, early_stop_callback])
 
     # Load data
     train_loader, test_loader, val_loader = get_fruits_and_vegetables_dataloaders()
@@ -41,3 +43,6 @@ if __name__ == "__main__":
     plot_placeholder_loss()
     print("Loss plot saved.")
     wandb.finish()
+
+if __name__ == "__main__":
+    typer.run(train_model)
